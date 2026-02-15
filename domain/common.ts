d6e121989c2172e4142a54424785ad9e971768b1
michealroberts <michael@observerly.com>
2026-02-15 13:11:13 +0000

    feat: add const ID_PREFIXES to common domain
    feat: add const ID_PREFIXES to common domain

---
+++ domain/common.ts
@@ -0,0 +1,19 @@
+/******************************************************************************/
+
+// @author         Michael J Roberts
+// @license        Copyright © 2021-2025 MTG Mythic Rare
+
+/******************************************************************************/
+
+// Define a constant object for ID prefixes to ensure consistency across the
+// codebase.
+export const ID_PREFIXES = {
+  set: "set",
+  oracle: "oracle",
+  card: "card",
+  face: "face",
+  ruling: "ruling",
+  price: "price",
+} as const;
+
+/******************************************************************************/
